3d8fe1c43f4ce156429e537e2d82c33769635e2e
王瑞瑞 <ruirui.wang@fengjr.comm>
2020-06-18 15:26:59 +0800

    fix

--- quanx/mi_fit.js
+++ quanx/mi_fit.js
@@ -7,15 +7,21 @@
 *.huami.com
 
 [rewrite_local]
-^https?://api-mifit-cn\.huami\.com/v1/data/band_data\.json url script-response-body https://raw.githubusercontent.com/ruicky/scripts/master/quanx/mi_fit.js
+^https?:\/\/api-mifit-cn\.huami\.com\/v1\/data\/band_data\.json url script-request-body https://raw.githubusercontent.com/ruicky/scripts/master/quanx/mi_fit.js
 
 */
+
+/**
+ * @fileoverview Example of HTTP rewrite.
+ *
+ * @supported Quantumult X (v1.0.5-build173)
+ */
 
 
 // 原理会在同步手环信息的时候修改步数
 
 var step =24510 // 可自定义
-var body = $response.body;
+var body = $request.body;
 
 body = body.replace(/ttl%5C%22%3A[0-9]*%2C%5C%22dis/g,'ttl%5C%22%3A'+step+'%2C%5C%22dis')
 
